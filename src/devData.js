f75bae9ef2db89aa19453077a402d095535d3cf1
Amy Gori <amydgori@gmail.com>
2022-07-19 13:04:56 -0400

    Add the actual dev data file

---
+++ src/devData.js
@@ -0,0 +1,16 @@
+const devs = [
+  {
+    name: 'Susan',
+    expertise: 'React, JavaScript, Music',
+  },
+  {
+    name: 'Rachel',
+    expertise: 'React, JavaSript, Yoga',
+  },
+  {
+    name: 'Daniela',
+    expertise: 'React, JavaSript, Yoga',
+  },
+]
+
+export default devs
